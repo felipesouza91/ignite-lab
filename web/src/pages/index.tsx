d7a617d5e359eaa836824a4228b4117c2f7e465b
Felipe Souza <felipedb91@gmail.com>
2022-04-05 22:18:33 -0300

    feat: create a home app page

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,12 +1,22 @@
-import { useUser } from '@auth0/nextjs-auth0';
+import { getSession } from '@auth0/nextjs-auth0';
+import { GetServerSideProps } from 'next';
 
 export default function Home() {
-  const { user } = useUser();
-  return (
-    <div>
-      <h1>Teste</h1>
-      <h2>{JSON.stringify(user)}</h2>
-      <a href="/api/auth/login/">Login</a>
-    </div>
-  );
+  return null;
 }
+
+export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
+  const session = getSession(req, res);
+  if (!session) {
+    return {
+      redirect: {
+        destination: '/api/auth/login',
+        permanent: false,
+      },
+    };
+  } else {
+    return {
+      redirect,
+    };
+  }
+};
